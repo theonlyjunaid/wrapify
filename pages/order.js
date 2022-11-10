1012268527aba0168c2d1dbb9c7a91520de771cc
junaid <onlyjunaid2002@gmail.com>
2022-11-10 20:18:22 +0530

    Gbdvcx

--- pages/order.js
+++ pages/order.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 import React from 'react'
 import Order from '../model/Order'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const order = ({ order, clearCart }) => {
 
@@ -12,13 +13,13 @@ const order = ({ order, clearCart }) => {
         }
     }
 
-
+// console.log(order)
 
     return (
         <div><section className="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-24 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                    <div className=" w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">
                             Wrapify
                         </h2>
@@ -28,7 +29,8 @@ const order = ({ order, clearCart }) => {
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">
                             Order has been successfully placed your payment is {order.status}
                         </h2>
-                        <div className="flex mb-4">
+                        <div className="grid grid-cols-4">
+                            <a href="" className='flex-grow border-b-2 border-gray-300 py-2 text-lg px-1'></a>
                             <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
                                 Description
                             </a>
@@ -40,12 +42,67 @@ const order = ({ order, clearCart }) => {
                             </a>
                         </div>
                         {Object.keys(order.products).map((item) => {
+                            const { name, price, quantity,img,size,varient,qty} = order.products[item]
+                            console.log(size)
+                            // console.log(name[0].split("(")[1].split("/"))
                             return (
-                                <div key={item} className='flex border-b border-gray-200 py-2'>
-                                    <span className="text-gray-500">{order.products[item].name}</span>
-                                    <span className="ml-auto text-gray-900">{order.products[item].qty}</span>
-                                    <span className="ml-auto text-gray-900">{order.products[item].price}</span>
+                                <div key={item} >
+                                    {
+                                        name.map((item,index)=>{
+                                            return(
+                                                <div key={name} className='grid grid-cols-4 items-center border-b-2 py-[4px] md:text-sm' >
+                                                    <div className='flex flex-col justify-center items-center relative w-[100px] h-[100px]'>
+                                                        <Image
+                                                            src={img}
+                                                            layout='fill'
+                                                            objectFit='contain'
+                                                            alt="Picture of the Skin"
+                                                        />
+                                                    </div>
+                                                
+
+                                                            <div>
+
+                                                            <p className='font-mono flex items-center'>{item} </p>
+                                                            {/* <p className='font-thin text-gray-800 my-[5px]'>{varient}</p> */}
+                                                            </div>
+                                                            <p className='font-mono'>{size[index].split(" ")[1]}</p>
+                                                            <div>
+                                                                <p className='font-mono'>{price}</p>
+                                                            </div>
+
+                                                    
+                                                </div>
+                                            )
+                                        }
+                                        )
+                                    }
                                 </div>
+                                // <div key={name} className='flex items-center border-b-2 py-[24px]' >
+                                //     <div className='flex flex-col justify-center items-center relative w-[240px] h-[240px]'>
+                                //         <Image
+                                //             src={img}
+                                //             layout='fill'
+                                //             objectFit='contain'
+                                //             alt="Picture of the Skin"
+                                //         />
+                                //     </div>
+                                //     <div>
+                                //         <div className='md:text-xl ml-3 md:ml-10 '>
+                                //             <p className='font-mono flex items-center'>{name} </p>
+                                //             <p className='font-thin text-gray-800 my-[5px]'>{varient}</p>
+                                //             <p className='font-mono'>Quantity - {qty}</p>
+                                //             <div>
+                                //                 <p className='font-mono'>Price - {price}</p>
+                                //             </div>
+                                //         </div>
+                                //     </div>
+                                // </div>
+                                // <div key={item} className='flex border-b border-gray-200 py-2'>
+                                //     <span className="text-gray-500">{order.products[item].name}</span>
+                                //     <span className="ml-auto text-gray-900">{order.products[item].qty}</span>
+                                //     <span className="ml-auto text-gray-900">{order.products[item].price}</span>
+                                // </div>
                             )
 
                         })}
@@ -53,7 +110,7 @@ const order = ({ order, clearCart }) => {
 
                         <div className="flex my-2 py-2">
                             <span className="title-font font-medium text-2xl text-gray-900">
-                                rs {order.amount}
+                                ₹{order.amount}
                             </span>
                             <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                                 Track Order
@@ -72,11 +129,11 @@ const order = ({ order, clearCart }) => {
                             </button>
                         </div>
                     </div>
-                    <img
+                    {/* <img
                         alt="ecommerce"
                         className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
                         src="https://dummyimage.com/400x400"
-                    />
+                    /> */}
                 </div>
             </div>
         </section>
