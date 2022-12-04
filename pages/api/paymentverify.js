0c4990e8390858f537d23e0e23928a03f4563275
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2022-12-04 09:08:05 +0530

    Update paymentverify.js

--- pages/api/paymentverify.js
+++ pages/api/paymentverify.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import Order from "../../model/Order";
 import connectDB from "../../middleware/mongoose";
 import crypto from "crypto";
-const mailgun = require("mailgun-js");
-const DOMAIN = process.env.MAILGUN_DOMAIN;
-const mg = mailgun({
-    apiKey: process.env.API_EMAIL, domain: DOMAIN
-});
+// const mailgun = require("mailgun-js");
+// const DOMAIN = process.env.MAILGUN_DOMAIN;
+// const mg = mailgun({
+//     apiKey: process.env.API_EMAIL, domain: DOMAIN
+// });
 
 
 
@@ -23,15 +23,15 @@ const handler = async (req, res) => {
     // console.log("sig" + expectedSignature)
     if (expectedSignature === razorpay_signature) {
         order = await Order.findOneAndUpdate({ 'paymentInfo.id': razorpay_order_id }, { status: 'paid', paymentInfo: req.body });
-        const data = {
-            from: 'Mz art <order@www.mzart.in>',
-            to: order.email,
-            subject: 'Order Placed',
-            html: '<h1>Your Order is been placed</h1>'
-        };
-        mg.messages().send(data, function (error, body) {
-            console.log(body);
-        });
+//         const data = {
+//             from: 'Mz art <order@www.mzart.in>',
+//             to: order.email,
+//             subject: 'Order Placed',
+//             html: '<h1>Your Order is been placed</h1>'
+//         };
+//         mg.messages().send(data, function (error, body) {
+//             console.log(body);
+//         });
         let products = order.products;
         for (let slug in products) {
             await mongoose.connection.db.collection('products').updateOne({ slug: slug }, { $inc: { availableQty: -products[slug].qty } });
