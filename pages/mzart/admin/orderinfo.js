f42dd473cdae77e3de7a087a7cdb2806fa738ee1
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2022-12-05 09:43:21 +0530

    Update orderinfo.js

--- pages/mzart/admin/orderinfo.js
+++ pages/mzart/admin/orderinfo.js
@@ -16,7 +16,6 @@ const Authenticate = (e) => {
   fetch('https://apiv2.shiprocket.in/v1/external/auth/login', {
       method: 'POST',
       headers: {
-        'Access-Control-Allow-Origin': 'https://coral-app-yj3q3.ondigitalocean.app',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
