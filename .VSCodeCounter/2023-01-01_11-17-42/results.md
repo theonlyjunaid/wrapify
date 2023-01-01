8923bb5db63eac0c2c32c336390358bf3ce3281f
junaid <onlyjunaid2002@gmail.com>
2023-01-01 11:28:44 +0530

    ews

---
+++ .VSCodeCounter/2023-01-01_11-17-42/results.md
@@ -0,0 +1,51 @@
+# Summary
+
+Date : 2023-01-01 11:17:42
+
+Directory /Users/junaid./Documents/GitHub/wrapify
+
+Total : 99 files,  392858 codes, 1474 comments, 1160 blanks, all 395492 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| JSON | 4 | 384,459 | 0 | 20 | 384,479 |
+| JavaScript | 69 | 6,033 | 903 | 753 | 7,689 |
+| HTML | 1 | 1,165 | 48 | 122 | 1,335 |
+| JavaScript React | 16 | 1,138 | 523 | 248 | 1,909 |
+| JSON with Comments | 1 | 20 | 0 | 1 | 21 |
+| Markdown | 1 | 18 | 0 | 11 | 29 |
+| XML | 6 | 18 | 0 | 0 | 18 |
+| CSS | 1 | 7 | 0 | 5 | 12 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 99 | 392,858 | 1,474 | 1,160 | 395,492 |
+| components | 15 | 1,032 | 519 | 233 | 1,784 |
+| components/Item | 5 | 223 | 154 | 58 | 435 |
+| components/admin | 1 | 66 | 1 | 6 | 73 |
+| components/home | 2 | 86 | 43 | 28 | 157 |
+| data | 3 | 830 | 0 | 34 | 864 |
+| middleware | 1 | 9 | 0 | 1 | 10 |
+| model | 6 | 82 | 0 | 10 | 92 |
+| pages | 57 | 5,167 | 902 | 715 | 6,784 |
+| pages/api | 25 | 1,564 | 176 | 238 | 1,978 |
+| pages/collection | 3 | 236 | 35 | 53 | 324 |
+| pages/collection/[design] | 1 | 65 | 15 | 14 | 94 |
+| pages/mzart | 2 | 206 | 6 | 31 | 243 |
+| pages/mzart/admin | 2 | 206 | 6 | 31 | 243 |
+| pages/products | 1 | 152 | 109 | 11 | 272 |
+| pages/skin | 7 | 495 | 412 | 123 | 1,030 |
+| pages/skin/[brands] | 6 | 422 | 407 | 107 | 936 |
+| pages/skin/[brands]/[models] | 5 | 373 | 337 | 91 | 801 |
+| pages/skin/[brands]/[models]/[design] | 1 | 113 | 286 | 34 | 433 |
+| pages/skin/[brands]/[models]/dadu | 2 | 17 | 0 | 5 | 22 |
+| public | 6 | 18 | 0 | 0 | 18 |
+| public/icons | 5 | 14 | 0 | 0 | 14 |
+| styles | 1 | 7 | 0 | 5 | 12 |
+| template | 1 | 1,165 | 48 | 122 | 1,335 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- pages/checkout.js
+++ pages/checkout.js
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import pincode from '../pincode.json'
+import { ProductReview } from '../components/checkout/ProductReview'
 // import mongoose from 'mongoose'
 // import Pincodes from '../model/Pincodes'
 
@@ -449,59 +450,9 @@ value={info.address1}
                     </div>
                     </div>
                 </div>
-                <div className='hidden md:block md:w-2/5 bg-gray-200  rounded-xl ml-4'>
-                    <div className="sidbar  p-4  py-10  w-[100%]  z-20">
-                        <div className='list-decimal text-semibold text-black'>
-                            {Object.keys(cart).length === 0 && <li className='my-4 font-normal'>Cart is empty</li>}
-                            {Object.keys(cart).map((key) => {
-                                const { name, price, qty, size, varient, img } = cart[key]
-
-                                return (
-                                    <div key={name} className='flex  border-b border-gray-400  py-[10px]' >
-                                        <div className='flex flex-col justify-center items-center relative w-[100px] h-[100px]'>
-                                            <Image
-                                            priority
-                                                src={img}
-                                                layout='fill'
-                                                objectFit='contain'
-                                                alt="Picture of the Skin"
-                                            />
-                                        </div>
-
-                                        <div className=' ml-1 md:ml-4  items-center w-3/5'>
-                                            <p className='font-mono flex items-center'>{name.split("(")[0]} </p>
-                                            <p className='font-thin text-gray-800 my-[5px]'>{varient}</p>
-                                            <div className='md:flex'>
-                                                <p className='font-thin text-gray-800 my-[5px]'>Size: {size} /</p>
-                                                <p className='font-thin text-gray-800 my-[5px] ml-2'>Qty: {qty}</p>
-                                            </div>
-
-                                        </div>
-
-                                        <div className=' ml-1 md:ml-4 flex items-center text-center w-1/5'>
-                                            {/* <p className='font-mono'>Price</p> */}
-                                            <p className='font-mono'>₹{price * qty}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <div className='flex justify-between items-center mt-4'>
-                            <p className='font-base'>Subtotal</p>
-                            <p className='font-semibold'>₹{subTotal}</p>
-                        </div>
-                        <div className='flex justify-between items-center mt-4'>
-                            <p className='font-base'>Delivery Charges</p>
-                            <p className='font-semibold'>₹{0}</p>
-                        </div>
-
-                        <div className='flex justify-between items-center mt-4 border-t border-gray-500 pt-4'>
-                            <p className='font-base'>Total Amount</p>
-                            <p className='font-semibold text-2xl'>₹{subTotal}</p>
-                        </div>
-
-                    </div>
-                </div>
+              
+    <ProductReview cart={cart} subTotal={subTotal}/>
+     
             </div>
 
         </div>
