7d48fe004b4411c2168ca5219d703e28ab868cea
junaid <onlyjunaid2002@gmail.com>
2023-02-15 22:46:06 +0530

    EFDSCXZ

--- data/mobile.js
+++ data/mobile.js
@@ -1,77 +1,13 @@
 export const mobile = {
     apple: {
         name: 'Apple',
-        models:{
-            iphone14series: {
-                name: 'iPhone 14 Series',
-                models: {
-                    "iphone14": {
-                        name: 'iPhone 14',
-                        URL: '/skin/apple/iphone-14',
-                    },
-                    iphone14pro: {
-                        name: 'iPhone 14 Pro',
-                        URL: '/skin/apple/iphone-14-pro',
-                },
-                iphone14proMax: {
-                    name: 'iPhone 14 Pro Max',
-                    URL: '/skin/apple/iphone-14-pro-max',
-                },
-                iphone14plus: {
-                    name: 'iPhone 14 Plus',
-                    URL: '/skin/apple/iphone-14-plus',
-                },
-
-            },
-        },
-        iphone13series: {
-            name: 'iPhone 13 Series',
-            models: {
-                iphone13: {
-                    name: 'iPhone 13',
-                    URL: '/skin/apple/iphone-13',
-                },
-                iphone13pro: {
-                    name: 'iPhone 13 Pro',
-                    URL: '/skin/apple/iphone-13-pro',
-                },
-                iphone13proMax: {
-                    name: 'iPhone 13 Pro Max',
-                    URL: '/skin/apple/iphone-13-pro-max',
-                },
-                iphone13plus: {
-                    name: 'iPhone 13 Plus',
-                    URL: '/skin/apple/iphone-13-plus',
-                },
-                iphone13mini: {
-                    name: 'iPhone 13 Mini',
-                    URL: '/skin/apple/iphone-13-mini',
-
-            },
-        },
-    }
-
-
-
-        }
+    
     },
-    samsung: {
-        name: 'Samsung',
-        models: {
-            galaxyS21series: {
-                name: 'Galaxy S22 Series',
-                models: {
-                    galaxyS22: {
-                        name: 'Galaxy S22',
-                        URL: '/skin/samsung/s22',
-                    },
-                    galaxyS21Ultra: {
-                        name: 'Galaxy S21 Ultra',
-                        URL: '/skin/samsung/s22-ultra',
-                    },
-                  }
-            },
-        }
+    oneplus: {
+        name: 'Oneplus',
+    },
+    nothing: {
+        name: 'nothing',
     },
 
 }
